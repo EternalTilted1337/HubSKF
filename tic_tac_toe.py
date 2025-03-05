1b7f53102a4f3eae82c0f890c9dc9b1a1aba04b8
Mihail <@anitikrev@gmail.com>
2025-03-05 16:05:20 +0300

    Для Сани

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,9 +1,15 @@
-print('Добро пожаловать в игру крестики нолики, обзовитесь')
-player_1 = input('Первый игрок обзовись:')
-player_2 = input('Первый второй обзовись: ')
-print('Правила игры очень просты, нужно по вертикали или горизонтали собрать линию.Кто первый собереёт тот и подебил!')
-print('Начинает первый игрок: ', player_1)
+#print('Добро пожаловать в игру крестики нолики, обзовитесь')
+#player_1_name = input('Первый игрок обзовись:')
+#player_2_name = input('Первый второй обзовись: ')
+#print('Правила игры очень просты, нужно по вертикали или горизонтали, либо по диагонали собрать линию.Кто первый собереёт тот и подебил!')
+#player_1 = int(input())
+#print('Начинает первый игрок: ', player_1)
+#player_2 = int(input())
+lst_board = [1,2,3,4,5,6,7,8,9]
+
+
 def board():
+
     a1= 0
     b2 = 3
     b3 = 6
@@ -15,13 +21,18 @@ def board():
         print("|", " " * 4,a1, '|', " " * 4, b2, '|', " " * 4,b3,'|')
     print('_' * 27)
 
-#def winner():
-    #lst_win = [1,2,3], [4,5,6], [7,8,9], [1,5,9], [3,5,7]
+def winner():
+    lst_win = [1,2,3], [4,5,6], [7,8,9], [1,5,9], [3,5,7]
 
-def motion_player_1_x():
+def motion_player_1_x(step):
     lst_motion_p1 = []
-def motion_player_2_o():
+    lst_motion_p1.append(step)
 
+def motion_player_2_o(step):
+    lst_motion_p2 = []
+    lst_motion_p2.append(step)
+
+def hod():
 
 def motion_player_1():
     lst_win = [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7]
@@ -38,4 +49,6 @@ def motion_player_2():
 
 board()
 
+#def motion_player_1():
 
+#def motion_player_2():
