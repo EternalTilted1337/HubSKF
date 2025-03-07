d69f96a7a72ee592cf98703aa78300d2fce1d6d6
Mihail <@anitikrev@gmail.com>
2025-03-07 17:23:54 +0300

    Бэкап

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,10 +1,12 @@
+from wsgiref.validate import check_exc_info
+
 print('Добро пожаловать в игру крестики нолики, обзовитесь')
 player_1_name = input('Первый игрок обзовись:')
 player_2_name = input('Первый второй обзовись: ')
 print('Правила игры очень просты, нужно по вертикали или горизонтали, либо по диагонали собрать линию.Кто первый собереёт тот и подебил!')
 
-print('Начинает первый игрок: ')
 
+Flag = True
 
 def board():
     a1= 0
@@ -29,79 +31,31 @@ step_player = {
     8: None,
     9: None,
 }
-
-def win():
-    #winner_lst = [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7]
-    g1 = [1,2,3]
-    g2 = [2,5,8]
-    g3 = [3,6,9]
-    v1 = [1,2,3]
-    v2 = [4,5,6]
-    v3 = [7,8,9]
-    d1 = [1,5,9]
-    d2 = [3,5,7]
-    flag = True
-    lst_step_player_1 = []
-    lst_step_player_2 = []
-    ln  = 0
-    for i, player in step_player.items():
-        if player == 'player_1':
-            lst_step_player_1.append(i)
-        elif player == 'player_2':
-            lst_step_player_2.append(i)
-
-
-    while flag:
-        if sorted(lst_step_player_1) in g1 or  g2 or  g3 or v1 or v2 or v3 or d1 or d2:
-            flag = False
-            print('Подебил ', player_1_name, 'игрок')
-            break
-        elif sorted(lst_step_player_2) in g1 or  g2 or  g3 or v1 or v2 or v3 or d1 or d2:
-            flag = False
-            print('Подебил ', player_2_name, 'игрок')
-            break
-
-                # else:
-        #elif player == None:
-            #continue
-
-#def motion_player_1_x(step):
-    #lst_motion_p1 = []
-    #lst_motion_p1.append(step)
+print('Начинает', player_1_name,  'игрок: ')
+# def win():
+#     #winner_lst = [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7]
+#     g1 = [1,2,3]
+#     g2 = [2,5,8]
+#     g3 = [3,6,9]
+#     v1 = [1,2,3]
+#     v2 = [4,5,6]
+#     v3 = [7,8,9]
+#     d1 = [1,5,9]
+#     d2 = [3,5,7]
+#
+#     lst_step_player_1 = []
+#     lst_step_player_2 = []
+#     ln  = 0
+#     for i, player in step_player.items():
+#         if player == 'player_1':
+#             lst_step_player_1.append(i)
+#         elif player == 'player_2':
+#             lst_step_player_2.append(i)
 
 
 
+#lst_board = [1,2,3,4,5,6,7,8,9]
 
-# step_player_1 = []
-# step_player_2 = []
-# s = int(input())
-# for s in range(1,10):
-#     p1 = 0
-#     p2 = 2
-#     if p1 == 0:
-#         step_player_1.append(s)
-#         p1 == 2
-#     if p1 == 2:
-#         step_player_2.append(s)
-#         p1 == 0
-
-
-
-player_1 = int(input())
-player_2 = int(input())
-player_1 = int(input())
-player_2 = int(input())
-player_1 = int(input())
-player_2 = int(input())
-player_1 = int(input())
-player_2 = int(input())
-player_1 = int(input())
-
-
-
-lst_board = [1,2,3,4,5,6,7,8,9]
-
-prv = True
 
 
 
@@ -110,14 +64,14 @@ prv = True
 
 def step_1():
     player_1 = int(input())
-    print(player_1)
+    #print(player_1)
     if player_1 > 9:
         print('Число слишком большое, введите число от 1-9')
         step_1()
     elif step_player[player_1] == None and step_player[player_1] != 'player1' and step_player[player_1] != 'player2':
         step_player[player_1] = 'player1'
-        print('Ходит игрок ', player_1)
-        print(step_player)
+        print('Ходит игрок ', player_1_name)
+        #print(step_player)
     else:
         print('Клетка уже занята')
         step_1()
@@ -125,31 +79,59 @@ def step_1():
 
 def step_2():
     player_2 = int(input())
-    print(player_2)
+    #print(player_2)
     if player_2 > 9:
         print('Число слишком большое, введите число от 1-9')
         step_2()
     elif step_player[player_2] == None and step_player[player_2] != 'player1' and step_player[player_2] != 'player2':
         step_player[player_2] = 'player2'
-        print('Ходит игрок ', player_2)
-        print(step_player)
+        print('Ходит игрок ', player_2_name)
     else:
         print('Клетка уже занята')
         step_2()
 
+def Check_Win():
+    global Flag
+    winner_lst = [1,2,3,], [4,5,6], [7,8,9],[1,5,9],[3,5,7], [1,4,7], [2,5,8], [3,6,9]
+    player1Score = []
+    player2Score = []
+    for i in step_player:
+        if step_player[i] == 'player1':
+            player1Score.append(i)
+        if step_player[i] == 'player2':
+            player2Score.append(i)
+    for arr  in winner_lst:
 
+        if len(player1Score) >= 3:
+            result1 = all((a == b for a in player1Score) for b in arr)
+            if result1:
+                print('Победил ', player_1_name)
+                Flag = False
+                return #Dlya chego?
 
+        if len(player2Score) >3:
+            result2 = all((a,b) for a in player2Score for b in arr)
+            if result2:
+                print('Победил', player_2_name)
+                Flag = False
+                return #Dlya chego?
 
-
-
-
-
-
-
-
-win()
-
-#board()
+while (Flag):
+    counter = 2
+    if counter // 2:
+        #print('Ходит игрок 1')#, player_1_name)
+        step_1()
+        Check_Win()
+        if Flag == False:
+            break
+        counter += 1
+    if counter // 3:
+        #print('Ходит игрок 2')#, player_2_name)
+        step_2()
+        Check_Win()
+        if Flag == False:
+            break
+        counter += 1
 
 #step1_step2()
 #def motion_player_1():
