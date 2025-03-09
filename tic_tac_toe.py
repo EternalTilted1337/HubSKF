42c7da02133885026631ec7274e2abf3e415ad12
Mihail <@anitikrev@gmail.com>
2025-03-09 05:16:30 +0300

    Так чисто посмотреть

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -20,6 +20,7 @@ def board():
         print("|", " " * 4,a1, '|', " " * 4, b2, '|', " " * 4,b3,'|')
     print('_' * 27)
 board()
+print(f'Ходит игрок {player_1_name}')
 step_player = {
     1: None,
     2: None,
@@ -31,46 +32,19 @@ step_player = {
     8: None,
     9: None,
 }
-print('Начинает', player_1_name,  'игрок: ')
-# def win():
-#     #winner_lst = [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7]
-#     g1 = [1,2,3]
-#     g2 = [2,5,8]
-#     g3 = [3,6,9]
-#     v1 = [1,2,3]
-#     v2 = [4,5,6]
-#     v3 = [7,8,9]
-#     d1 = [1,5,9]
-#     d2 = [3,5,7]
-#
-#     lst_step_player_1 = []
-#     lst_step_player_2 = []
-#     ln  = 0
-#     for i, player in step_player.items():
-#         if player == 'player_1':
-#             lst_step_player_1.append(i)
-#         elif player == 'player_2':
-#             lst_step_player_2.append(i)
-
-
-
-#lst_board = [1,2,3,4,5,6,7,8,9]
-
-
-
-
 
 
 
 def step_1():
+    #print('Ходит игрок', player_1_name)
     player_1 = int(input())
     #print(player_1)
     if player_1 > 9:
         print('Число слишком большое, введите число от 1-9')
         step_1()
     elif step_player[player_1] == None and step_player[player_1] != 'player1' and step_player[player_1] != 'player2':
-        step_player[player_1] = 'player1'
-        print('Ходит игрок ', player_1_name)
+        step_player[player_1] = 'player1' #добавляет ли в словарь????
+        print('Ходит игрок ', player_2_name)
         #print(step_player)
     else:
         print('Клетка уже занята')
@@ -78,6 +52,7 @@ def step_1():
 
 
 def step_2():
+    #print('Ходит игрок', player_2_name)
     player_2 = int(input())
     #print(player_2)
     if player_2 > 9:
@@ -85,16 +60,19 @@ def step_2():
         step_2()
     elif step_player[player_2] == None and step_player[player_2] != 'player1' and step_player[player_2] != 'player2':
         step_player[player_2] = 'player2'
-        print('Ходит игрок ', player_2_name)
+        print('Ходит игрок ', player_1_name)
     else:
         print('Клетка уже занята')
         step_2()
 
 def Check_Win():
     global Flag
-    winner_lst = [1,2,3,], [4,5,6], [7,8,9],[1,5,9],[3,5,7], [1,4,7], [2,5,8], [3,6,9]
+    winner_lst = [1,2,3], [4,5,6], [7,8,9],[1,5,9],[3,5,7], [1,4,7], [2,5,8], [3,6,9]
     player1Score = []
     player2Score = []
+    lst_check = []
+    char_check = 0
+
     for i in step_player:
         if step_player[i] == 'player1':
             player1Score.append(i)
@@ -115,25 +93,22 @@ def Check_Win():
                 print('Победил', player_2_name)
                 Flag = False
                 return #Dlya chego?
+    for couple in step_player.values():
+        if couple != None:
+            char_check += 1
+            lst_check.append(char_check)
+            chekc_len = len(lst_check)
+            if chekc_len == 9:
+                print('Ничья')
 
 while (Flag):
-    counter = 2
-    if counter // 2:
-        #print('Ходит игрок 1')#, player_1_name)
-        step_1()
-        Check_Win()
-        if Flag == False:
-            break
-        counter += 1
-    if counter // 3:
-        #print('Ходит игрок 2')#, player_2_name)
-        step_2()
-        Check_Win()
-        if Flag == False:
-            break
-        counter += 1
 
-#step1_step2()
-#def motion_player_1():
+    step_1()
+    Check_Win()
+    if Flag == False:
+        break
 
-#def motion_player_2():
+    step_2()
+    Check_Win()
+    if Flag == False:
+        break
